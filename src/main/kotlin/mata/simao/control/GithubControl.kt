2b40a6e01a2b2c8c3cd9d2ac3a4eb37ff1d4bab4
Simão Mata <simao.p.mata@gmail.com>
2023-08-24 00:05:11 +0100

    feat: add pagination support to obtain all values

--- src/main/kotlin/mata/simao/control/GithubControl.kt
+++ src/main/kotlin/mata/simao/control/GithubControl.kt
@@ -6,6 +6,7 @@ import mata.simao.control.mapper.RepositoryMapper
 import mata.simao.entity.Branch
 import mata.simao.entity.Repository
 import mata.simao.integration.GithubService
+import mata.simao.integration.entity.RepositoryDto
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.eclipse.microprofile.rest.client.inject.RestClient
 
@@ -17,22 +18,39 @@ class GithubControl(
     private val repositoryMapper: RepositoryMapper,
     private val branchMapper: BranchMapper
 ) {
-    fun getListOfRepositoriesByUsername(username: String): List<Repository> {
-        return githubService
-            .getUserRepositories(authorizationHeaderParam, username, "100", "1")
-            .filter { it.fork == false || it.fork == null }
-            .map { repositoryMapper.buildRepositoryResponse(it) }
-            .map { it.also { repo -> repo.branches = getListOfBranchesByRepository(repo) } }
+    companion object {
+        const val PER_PAGE_VALUE = 1
     }
 
-    private fun getListOfBranchesByRepository(repo: Repository): List<Branch> =
-        githubService
-            .getRepositoryBranches(
-                authorizationHeaderParam,
-                repo.ownerLogin ?: "",
-                repo.repositoryName ?: "",
-                "100",
-                "1"
-            )
+    fun getListOfRepositoriesByUsername(username: String): List<Repository> {
+        return getAllUserRepositories(username)
+            .filter { it.fork == false || it.fork == null }
+            .map { repositoryMapper.buildRepositoryResponse(it) }
+            .map { it.also { repo -> repo.branches = getAllRepositoryBranches(repo) } }
+    }
+
+    private fun getAllUserRepositories(username: String): List<RepositoryDto> =
+        generateSequence(1) { it + 1 }
+            .map { pageNr ->
+                githubService.getUserRepositories(authorizationHeaderParam, username, PER_PAGE_VALUE, pageNr)
+            }
+            .takeWhile { it.isNotEmpty() }
+            .flatten()
+            .toList()
+
+    private fun getAllRepositoryBranches(repo: Repository): List<Branch> =
+        generateSequence(1) { it + 1 }
+            .map { pageNr ->
+                githubService.getRepositoryBranches(
+                    authorizationHeaderParam,
+                    repo.ownerLogin,
+                    repo.repositoryName,
+                    PER_PAGE_VALUE,
+                    pageNr
+                )
+            }
+            .takeWhile { it.isNotEmpty() }
+            .flatten()
             .map { branchMapper.buildBranchResponse(it) }
+            .toList()
 }
