b0478ed13924c444ea6975ec14ce8c41c0e2e172
Simão Mata <simao.p.mata@gmail.com>
2023-08-24 00:54:37 +0100

    feat: finish unit tests

--- src/test/kotlin/mata/simao/control/mapper/BranchMapperTest.kt
+++ src/test/kotlin/mata/simao/control/mapper/BranchMapperTest.kt
@@ -1,2 +1,34 @@
 package mata.simao.control.mapper
 
+import mata.simao.integration.entity.BranchCommitDto
+import mata.simao.integration.entity.BranchDto
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+internal class BranchMapperTest {
+
+    private val branchMapper = BranchMapper()
+
+    @Test
+    fun `should map all fields`() {
+        val branchCommitDto = BranchCommitDto(sha = "123")
+        val branchDto = BranchDto(name = "testBranchName", branchCommitDto)
+
+        val branch = branchMapper.buildBranchResponse(branchDto)
+
+        assertEquals("testBranchName", branch.name)
+        assertEquals("123", branch.lastCommitSha)
+    }
+
+    @Test
+    fun `should not map fields`() {
+        val branchDto = BranchDto()
+
+        val branch = branchMapper.buildBranchResponse(branchDto)
+
+        assertNull(branch.name)
+        assertNull(branch.lastCommitSha)
+    }
+
+}
--- src/test/kotlin/mata/simao/control/mapper/RepositoryMapperTest.kt
+++ src/test/kotlin/mata/simao/control/mapper/RepositoryMapperTest.kt
@@ -1,2 +1,35 @@
 package mata.simao.control.mapper
 
+import mata.simao.integration.entity.RepositoryDto
+import mata.simao.integration.entity.RepositoryOwnerDto
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+internal class RepositoryMapperTest {
+
+    private val repositoryMapper = RepositoryMapper()
+
+    @Test
+    fun `should map all fields except branches`() {
+        val repositoryOwnerDto = RepositoryOwnerDto(login = "testOwnerName")
+        val repositoryDto = RepositoryDto(name = "testRepositoryName", owner = repositoryOwnerDto, fork = true)
+
+        val repository = repositoryMapper.buildRepositoryResponse(repositoryDto)
+
+        assertEquals("testRepositoryName", repository.repositoryName)
+        assertEquals("testOwnerName", repository.ownerLogin)
+        assert(repository.branches!!.isEmpty())
+    }
+
+    @Test
+    fun `should not map fields`() {
+        val repositoryDto = RepositoryDto()
+        val repository = repositoryMapper.buildRepositoryResponse(repositoryDto)
+
+        assertNull(repository.repositoryName)
+        assertNull(repository.ownerLogin)
+        assert(repository.branches!!.isEmpty())
+    }
+
+}
